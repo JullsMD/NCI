08afc9e4f2e3477290090df625789952787af5c9
Yuliia Melnyk <julls.hm.md@gmail.com>
2022-02-19 18:48:43 +0200

    исходники

---
+++ js/sliders.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // инициализация 1 слайдера с нужными настройками
+    new SimpleAdaptiveSlider('#slider-1', {
+      loop: true,
+      autoplay: false,
+      interval: 5000,
+      swipe: true,
+    });
+    new SimpleAdaptiveSlider('#slider-2', {
+loop: true,
+autoplay: false,
+interval: 5000,
+swipe: true,
+});
+new SimpleAdaptiveSlider('#slider-3', {
+loop: true,
+autoplay: false,
+interval: 5000,
+swipe: true,
+});
+new SimpleAdaptiveSlider('#slider-4', {
+loop: true,
+autoplay: false,
+interval: 5000,
+swipe: true,
+});
+new SimpleAdaptiveSlider('#slider-5', {
+loop: true,
+autoplay: false,
+interval: 5000,
+swipe: true,
+});
+new SimpleAdaptiveSlider('#slider-6', {
+loop: true,
+autoplay: false,
+interval: 5000,
+swipe: true,
+});
+new SimpleAdaptiveSlider('#slider-7', {
+loop: true,
+autoplay: false,
+interval: 5000,
+swipe: true,
+});
+new SimpleAdaptiveSlider('#slider-8', {
+loop: true,
+autoplay: false,
+interval: 5000,
+swipe: true,
+});
+  });
